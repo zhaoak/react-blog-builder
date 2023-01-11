9013fa7a6aa1b98514b59e23d6441dc93e5d8101
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-11 12:33:04 -0800

    all state variables defined in Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useState } from 'react';
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
 
@@ -7,7 +7,11 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-
+  const [title, setTitle] = useState('Title');
+  const [subtitle, setSubtitle] = useState('subtitle');
+  const [font, setFont] = useState('Comforter');
+  const [alignment, setAlignment] = useState('center');
+  const [text, setText] = useState('Your post text goes here');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
